967e6fcd670919152b68f0c0d5da1b9fdc831afa
Hayzival <bryanblindorard@gmail.com>
2022-11-13 17:07:47 +0100

    ajout de la connexion a la bd

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Client, Collection } = require('discord.js');
 const dotenv = require('dotenv'); dotenv.config();
 const client = new Client({ intents: 515 });
 const Logger = require('./utils/Logger');
-
+const mysql = require('mysql');
 
 ['commands', 'buttons', 'selects'].forEach(x => client[x] = new Collection());
 ['CommandUtil', 'EventUtil', 'ButtonUtil', 'SelectUtil'].forEach(handler => { require(`./utils/handlers/${handler}`)(client) });
@@ -18,5 +18,18 @@ process.on('unhandledRejection', (reason, promise) => {
     console.log(promise)
 })
 process.on('warning',(...args) => Logger.warn(...args))
+
+const db = new mysql.createConnection({
+    host : process.env.MYSQL_HOST,
+    user : process.env.MYSQL_USER,
+    password : process.env.MYSQL_PASSWORD,
+    database : process.env.MYSQL_DATABASE
+})
+
+db.connect(function (err) {
+    if (err) throw err;
+    Logger.client(`Connecté à la base de données MySQL!`)   
+})
+
 
 client.login(process.env.DISCORD_TOKEN);

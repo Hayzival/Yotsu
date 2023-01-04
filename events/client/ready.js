c3662542f0c94abf99705add89dec615a80ea8bf
BryanBlinDorard <brybrybbd@gmail.com>
2023-01-04 20:20:19 +0100

    ajout de la présence (a modif)

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,10 +1,23 @@
+const { ActivityType } = require('discord.js')
 const Logger = require('../../utils/Logger')
 
 module.exports= {
     name: 'ready',
     once: true,
-    async execute(client) {
-        Logger.client("- Je suis prêt")
+    async execute(client) {                
+
+        let guildsCount = await client.guilds.fetch()
+        let usersCount = await client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)
+
+        Logger.client(`- Je suis prêt à exterminé les ${usersCount} utilisateurs de discord sur les ${guildsCount.size} serveurs !`)
+
+        client.user.setPresence({
+            status: 'online',
+            activity: {
+                name: 'Exterminer les utilisateurs de discord',
+                type: ActivityType.WATCHING
+            }
+        })
 
         const devGuild = await client.guilds.cache.get('595272575060279316')
         devGuild.commands.set(client.commands.map(cmd => cmd))
